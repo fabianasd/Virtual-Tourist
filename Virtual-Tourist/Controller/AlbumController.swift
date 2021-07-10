c5d745ddf198cf34aa3e97f2ec888207f09a95c2
Fabiana Petrovick <fabypetrovick@gmail.com>
2021-07-10 00:16:38 -0300

    feat: Criação do AlbumController

--- Virtual-Tourist/Controller/AlbumController.swift
+++ Virtual-Tourist/Controller/AlbumController.swift
@@ -11,11 +11,60 @@ import UIKit
 import MapKit
 import CoreData
 
-class AlbumController: UIViewController {
-    @IBOutlet weak var AlbumPhotos: UIStackView!
+class AlbumController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    @IBOutlet weak var albumPhotos: UITableView!
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var editingMap:MapModel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.mapView.delegate = self
+    }
+    
     
     
     @IBAction func returnMap(_ sender: Any) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    func mapViewDidFinishRenderingMap(_ mapView: MKMapView, fullyRendered: Bool) {
+        addPinToMap()
+    }
+    // MARK: - MKMapViewDelegate
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        let reuseId = "pin"
+        
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+        
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = true
+            pinView!.pinTintColor = .red
+            pinView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        }
+        else {
+            pinView!.annotation = annotation
+        }
+        
+        return pinView
+    }
+    
+    func addPinToMap() {
+        //        let lat = CLLocationDegrees(editingMap.latitude as! Double)
+        //        let long = CLLocationDegrees(editingMap.longitude as! Double)
+        //
+        //        let coordinate = CLLocationCoordinate2D(latitude: lat, longitude: long)
+        
+        //        let first = editingMap.firstName as! String
+        //        let last = editingMap.lastName as! String
+        //        let mediaURL = editingMap.mediaURL as! String
+        
+        let annotation = MKPointAnnotation()
+        //        annotation.coordinate = coordinate
+        
+        self.mapView.addAnnotation(annotation)
+    //    self.mapView.showAnnotations(self.mapView.annotations, animated: true)
     }
 }
